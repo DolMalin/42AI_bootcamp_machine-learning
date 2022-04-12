64ad83bd4fdb8f7a11d748cc2807b1083e38fa61
DolMalin <paul.gl.dalmolin@gmail.com>
2022-04-12 12:29:31 +0200

    truc

--- module00/ex00/matrix.py
+++ module00/ex00/matrix.py
@@ -6,7 +6,7 @@
 #    By: pdal-mol <dolmalinn@gmail.com>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/04/12 10:54:32 by pdal-mol          #+#    #+#              #
-#    Updated: 2022/04/12 12:03:30 by pdal-mol         ###   ########.fr        #
+#    Updated: 2022/04/12 12:28:27 by pdal-mol         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,16 +19,20 @@ class Matrix:
 			self.shape = input
 			self.data = [[0.0] * input[1]] * input[0]
 		elif (isinstance(input, list)):
-				for lst in input:
-					assert len(input[0]) == len(lst)
-					for lst_elem in lst:
-						assert isinstance(lst_elem, float)
-				self.shape = (len(input), len(input[0]))
-				self.data = input
-				
-				
-						
+			for lst in input:
+				assert len(input[0]) == len(lst)
+				for lst_elem in lst:
+					assert isinstance(lst_elem, float)
+			self.shape = (len(input), len(input[0]))
+			self.data = input
+			
+	def T(self):
+		self.shape = (self.shape[1], self.shape[0])
+		return self
+		
 
-truc = Matrix([[1.0, 2.0], [3.0, 4.0]])
-
+			
+truc = Matrix([[0.0, 1.0], [2.0, 3.0], [4.0, 5.0]])
 print(truc.data, truc.shape)
+
+print(truc.T().shape)
