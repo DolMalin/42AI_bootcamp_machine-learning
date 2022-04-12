ba05256f9b557ce557febf60d80cc3b492dd10d7
DolMalin <paul.gl.dalmolin@gmail.com>
2022-04-12 12:03:59 +0200

    init matrice

---
+++ module00/ex00/matrix.py
@@ -0,0 +1,34 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    matrix.py                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: pdal-mol <dolmalinn@gmail.com>             +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/04/12 10:54:32 by pdal-mol          #+#    #+#              #
+#    Updated: 2022/04/12 12:03:30 by pdal-mol         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+class Matrix:
+	def __init__(self, input):
+		assert isinstance(input, tuple) or isinstance(input, list)
+		if (isinstance(input, tuple)):
+			assert len(input) == 2
+			assert isinstance(input[0], int) and isinstance(input[1], int)
+			self.shape = input
+			self.data = [[0.0] * input[1]] * input[0]
+		elif (isinstance(input, list)):
+				for lst in input:
+					assert len(input[0]) == len(lst)
+					for lst_elem in lst:
+						assert isinstance(lst_elem, float)
+				self.shape = (len(input), len(input[0]))
+				self.data = input
+				
+				
+						
+
+truc = Matrix([[1.0, 2.0], [3.0, 4.0]])
+
+print(truc.data, truc.shape)
