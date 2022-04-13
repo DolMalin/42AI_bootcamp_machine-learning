70122e8914f077f76025b27ca2c55d50382f2ef6
DolMalin <paul.gl.dalmolin@gmail.com>
2022-04-13 20:02:21 +0200

    clean code

--- module00/ex00/matrix.py
+++ module00/ex00/matrix.py
@@ -12,132 +12,155 @@
 
 # ============== ~ UTILS ~ ============== #
 
-def	create_matrix(row, col):
-	return [[0.0 for x in range (col)] for y in range (row)]
+def create_matrix(row, col):
+	return [[0.0 for x in range(col)] for y in range(row)]
 
-def	align_row(matrix, row):
+
+def col_to_row(matrix, row):
 	return [matrix[x][row] for x in range(len(matrix))]
 
-def	dot_product(a, b):
+
+def dot_product(a, b):
 	return sum(x*y for x, y in zip(a, b))
+
 
 # ============== ~ MATRICES OPERATIONS ~ ============== #
 
 def add_matrices(a, b):
 	result = create_matrix(len(a), len(a[0]))
-	for i in range (len(a)):
-		for j in range (len(a[0])):
+	for i in range(len(a)):
+		for j in range(len(a[0])):
 			result[i][j] = a[i][j] + b[i][j]
 	return result
-	
+
+
 def add_vectors(a, b):
 	if a.shape == b.shape:
 		result = create_matrix(len(a), len(a[0]))
-		for i in range (len(a)):
-			for j in range (len(a[0])):
+		for i in range(len(a)):
+			for j in range(len(a[0])):
 				result[i][j] = a[i][j] + b[i][j]
 		return result
 
+
 def sub_matrices(a, b):
 	result = create_matrix(len(a), len(a[0]))
-	for i in range (len(a)):
-		for j in range (len(a[0])):
+	for i in range(len(a)):
+		for j in range(len(a[0])):
 			result[i][j] = a[i][j] - b[i][j]
 	return result
 
+
 def div_matrix_scalar(a, b):
 	result = create_matrix(len(a), len(a[0]))
-	for i in range (len(a)):
-		for j in range (len(a[0])):
+	for i in range(len(a)):
+		for j in range(len(a[0])):
 			result[i][j] = a[i][j] / b
 	return result
 
+
 def mul_matrix_scalar(a, b):
 	result = create_matrix(len(a), len(a[0]))
-	for i in range (len(a)):
-		for j in range (len(a[0])):
+	for i in range(len(a)):
+		for j in range(len(a[0])):
 			result[i][j] = a[i][j] * b
 	return result
 
-def	mul_matrix_vector(a, b):
+
+def mul_matrix_vector(a, b):
 	if len(b) > 1:
 		result = create_matrix(len(a), len(b[0]))
-		for i in range (len(a)):
+		for i in range(len(a)):
 			sum = 0
 			for j in range(len(b)):
 				sum += a[i][j] * b[j][0]
 			result[i][0] = sum
 		return result
 	else:
-		result = create_matrix(len(a), len(b))	
-		for i in range (len(a)):
+		result = create_matrix(len(a), len(b))
+		for i in range(len(a)):
 			sum = 0
 			for j in range(len(b[0])):
 				sum += a[i][j] * b[0][j]
 			result[i][0] = sum
 		return result
 
+
 def mul_matrices(a, b):
 	result = create_matrix(len(a), len(b[0]))
-	for i in range (len(a)):
+	for i in range(len(a)):
 		for j in range(len(b[0])):
-			result[i][j] = dot_product(a[i], align_row(b, j))
+			result[i][j] = dot_product(a[i], col_to_row(b, j))
 	return (result)
+
+
+def mul_vectors(a, b):
+	v1 = [col_to_row(a, 0)] if len(a) > 1 else a
+	v2 = [col_to_row(b, 0)] if len(b) > 1 else a
+	return (dot_product(v1[0], v2[0]))
+
 
 # ============== ~ CLASSES ~ ============== #
 
+
 class Matrix:
 	def __init__(self, input):
-		assert isinstance(input, tuple) or isinstance(input, list)
-		if (isinstance(input, tuple)):
+		assert type(input) == tuple or type(input) == list
+		if (type(input) == tuple):
 			assert len(input) == 2
-			assert isinstance(input[0], int) and isinstance(input[1], int)
+			assert type(input[0]) == int and type(input[1]) == int
 			self.shape = input
 			self.data = create_matrix(input[0], input[1])
-		elif (isinstance(input, list)):
+		else:
 			for lst in input:
 				assert len(input[0]) == len(lst)
 				for lst_elem in lst:
-					assert isinstance(lst_elem, float)
+					assert type(lst_elem) == float
 			self.shape = (len(input), len(input[0]))
 			self.data = input
 
 	def __add__(self, other):
 		if type(self) == Matrix and type(other) == Matrix:
-			assert isinstance(other, type(self) and self.shape == other.shape)
+			assert self.shape == other.shape
 			return Matrix(add_matrices(self.data, other.data))
 		elif type(self) == Vector and type(other) == Vector:
 			return Vector(add_matrices(self.data, other.data))
-	
+
 	__radd__ = __add__
-	
+
 	def __sub__(self, other):
-		assert isinstance(other, type(self) and self.shape == other.shape)
+		assert type(other) == type(self) and self.shape == other.shape
 		return Matrix(sub_matrices(self.data, other.data))
 
 	def __rsub__(self, other):
-		assert isinstance(other, type(self) and self.shape == other.shape)
+		assert type(other) == type(self) and self.shape == other.shape
 		return Matrix(sub_matrices(other.data, self.data))
 
 	def __truediv__(self, other):
-		assert isinstance(other, int) or isinstance(other, float)
+		assert type(other) == int or type(other) == float
 		return Matrix(div_matrix_scalar(self.data, other))
 
 	__rtruediv__ = __truediv__
 
 	def __mul__(self, other):
-		if isinstance(other, int) or isinstance(other, float):
+		if type(other) == int or type(other) == float:
 			return Matrix(mul_matrix_scalar(self.data, other))
 		elif type(other) == Matrix and type(self) == Matrix:
-			assert self.shape[1] == other.shape[0] 
+			assert self.shape[1] == other.shape[0]
 			return (Matrix(mul_matrices(self.data, other.data)))
 		elif type(self) == Matrix and type(other) == Vector:
 			return (Matrix(mul_matrix_vector(self.data, other.data)))
 
+	__rmul__ = __mul__
+
+	def __str__(self):
+		return '<Matrix (shape=' + str(self.shape) + ', data=' + str(self.data) + ')'
+
+	__repr__ = __str__
+
 	def T(self):
-		self.shape = (self.shape[1], self.shape[0])
-		self.data = [list(row) for row in (zip(*self.data))]
-		return self
+		return Matrix([list(row) for row in (zip(*self.data))])
+
 
 class Vector(Matrix):
 	def __init__(self, input):
@@ -152,9 +175,6 @@ class Vector(Matrix):
 			self.data = input
 			self.shape = (1, len(input[0]))
 
-v1 = Vector([[1], [2], [3]])
-v2 = Vector([[2], [4], [8]])
-print((v1 + v2).data)
-# Output:
-# Output:
-# Matrix([[8], [16]])
+	def dot(self, other):
+		assert type(other) == Vector
+		return mul_vectors(self.data, other.data)
--- module00/ex00/test.py
+++ module00/ex00/test.py
@@ -1,37 +1,6 @@
+from matrix import Matrix, Vector
 
-def	create_matrix(row, col):
-	return [[0.0 for x in range (col)] for y in range (row)]
+A = Matrix([[2.0, 4.0], [6.0, 8.0]])
+B = Matrix([[2.0, 2.0, 2.0], [2.0, 2.0, 2.0]])
 
-def add_matrices(a, b):
-	result = create_matrix(len(a), len(a[0]))
-	print(result)
-	for i in range (len(a)):
-		for j in range (len(a[0])):
-			result[i][j] = a[i][j] + b[i][j]
-	return result
-
-
-def	align_row(matrix, row):
-	return [matrix[x][row] for x in range(len(matrix))]
-
-def	dot_product(a, b):
-	return sum(x*y for x, y in zip(a, b))
-
-def mul_matrices(a, b):
-	result = create_matrix(len(a), len(b[0]))
-	for i in range (len(a)):
-		for j in range(len(b[0])):
-			result[i][j] = dot_product(a[i], align_row(b, j))
-	return (result)
-
-# assert self.shape[1] == other.shape[0] 
-# result = create_matrix(self.shape[0], other.shape[1])
-# for i in range (self.shape[0]):
-# 	for j in range(other.shape[1]):
-# 		row = self.data[i]
-# 		col = [other.data[x][j] for x in range(len(other.data))]
-# 		result[i][j] = sum(x*y for x, y in zip(row, col)) 
-
-m1 = [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]
-m2 = [[7.0, 8.0], [9.0, 10.0], [11.0, 12.0]]
-print(mul_matrices(m1, m2))
+print(A)
