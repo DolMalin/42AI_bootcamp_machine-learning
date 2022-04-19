60fe82298333edb52403a365beb30149fd956e27
DolMalin <paul.gl.dalmolin@gmail.com>
2022-04-19 16:34:04 +0200

    import previous ft

--- module00/ex04/prediction.py
+++ module00/ex04/prediction.py
@@ -1,16 +1,7 @@
 import numpy as np
-
-def add_intercept(x):
-	if type(x) != np.ndarray or len(x) < 1:
-		return None
-	if type(x[0]) != np.ndarray:
-		x = x.reshape(-1, 1)
-	output = [[0.0 for x in range(len(x[0]) + 1)] for y in range(len(x))]
-	for i in range (len(x)):
-		for j in range (len(x[0])):
-			output[i][0] = 1.0
-			output[i][j + 1] = x[i][j] 
-	return(np.array(output))
+import sys
+sys.path.append('../')
+from ex03.tools import add_intercept
 
 def predict_(x, theta):
 	assert type(x) == np.ndarray and len(x) > 1
